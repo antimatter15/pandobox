cf2d891f5b5e6e6a5e70c6f0d12602ab29e941b3
antimatter15 <antimatter15@gmail.com>
2012-05-28 20:50:32 -0400

    first version

---
+++ proxy.php
@@ -0,0 +1,28 @@
+<?php
+/*
+function callback($buffer){
+	$fp = fopen("blah_page.txt", "w");
+	fwrite($fp, $buffer);
+	fclose($fp);	
+	return $buffer;
+}
+
+ob_start("callback");*/
+ob_start();
+header("Content-Type: application/octet-stream");
+$ch = curl_init($_REQUEST['url']);
+
+curl_setopt($ch, CURLOPT_HEADER, 0);
+//curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+//curl_setopt($ch, CURLOPT_FILE, STDOUT);
+
+$data = curl_exec($ch);
+
+curl_close($ch);
+//ob_end_flush();
+$filename = $_REQUEST['artist'] . " - " . $_REQUEST['song'] . ".mp3";
+$fp = fopen($filename, "w");
+fwrite($fp, ob_get_contents());
+fclose($fp);	
+chmod($filename, 0666);
+?>
