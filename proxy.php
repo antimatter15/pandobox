5043b991537f55b2f356b1a7e343a156fb871c82
antimatter15 <antimatter15@gmail.com>
2012-05-28 21:05:07 -0400

    added file existence check

--- proxy.php
+++ proxy.php
@@ -1,13 +1,6 @@
 <?php
-/*
-function callback($buffer){
-	$fp = fopen("blah_page.txt", "w");
-	fwrite($fp, $buffer);
-	fclose($fp);	
-	return $buffer;
-}
 
-ob_start("callback");*/
+ob_start("callback");
 ob_start();
 header("Content-Type: application/octet-stream");
 $ch = curl_init($_REQUEST['url']);
@@ -21,8 +14,12 @@ $data = curl_exec($ch);
 curl_close($ch);
 //ob_end_flush();
 $filename = $_REQUEST['artist'] . " - " . $_REQUEST['song'] . ".mp3";
-$fp = fopen($filename, "w");
-fwrite($fp, ob_get_contents());
-fclose($fp);	
-chmod($filename, 0666);
+
+if(!file_exists($filename)){
+	$fp = fopen($filename, "w");
+	fwrite($fp, ob_get_contents());
+	fclose($fp);	
+	chmod($filename, 0666);
+}
+
 ?>
